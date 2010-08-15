0659e8633d37e1b9651f46dac4e04c1d7e247143
Jeroen van Dijk <jeroen@jeevidee.nl>
2010-08-15 22:26:55 +0200

    Added first specs for remote session

---
+++ spec/session/remote_mechanize_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe Capybara::Session do
+  context 'with remote mechanize driver' do
+    before(:each) do
+      Capybara.app_host = "http://capybara-testapp.heroku.com"
+    end
+
+    after(:each) do
+      Capybara.app_host = nil
+    end
+    
+    
+    before do      
+      @session = Capybara::Session.new(:mechanize, TestApp)
+    end
+
+    describe '#driver' do
+      it "should be a mechanize driver" do
+        @session.driver.should be_an_instance_of(Capybara::Driver::Mechanize)
+      end
+    end
+
+    describe '#mode' do
+      it "should remember the mode" do
+        @session.mode.should == :mechanize
+      end
+    end
+
+    describe '#click_link' do
+      it "should use data-method if available" do
+        pending "Needs to be implemented" do
+          @session.visit "/with_html"
+          @session.click_link "A link with data-method"
+          @session.body.should == 'The requested object was deleted'
+        end
+      end
+    end
+
+    # Pending: Still 12 failing tests here:
+    # it_should_behave_like "session"
+
+    it_should_behave_like "session without javascript support"
+    it_should_behave_like "session with headers support"
+    it_should_behave_like "session with status code support"
+  end
+end
