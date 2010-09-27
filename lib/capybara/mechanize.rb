7fca12e0e43432faa864f11dfc89f4690431746c
Fred Wu <fred@envato.com>
2010-09-27 11:04:24 +1000

    Conform to the latest Capybara changes.

--- lib/capybara/mechanize.rb
+++ lib/capybara/mechanize.rb
@@ -1,1 +1,9 @@
-require 'capybara/driver/mechanize'
+module Capybara
+  module Driver
+    autoload :Mechanize, 'capybara/driver/mechanize_driver'
+  end
+end
+
+Capybara.register_driver :mechanize do |app|
+  Capybara::Driver::Mechanize.new(app)
+end
