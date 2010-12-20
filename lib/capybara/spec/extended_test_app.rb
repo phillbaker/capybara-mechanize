1d69434a4c98a46dbf93804d20a6e55512ad83c9
Jon Gilbraith <jon@professionalnerd.com>
2010-12-20 16:51:19 +0000

    Clean up excess whitespace.

--- lib/capybara/spec/extended_test_app.rb
+++ lib/capybara/spec/extended_test_app.rb
@@ -2,25 +2,25 @@ require 'capybara/spec/test_app'
 
 class ExtendedTestApp < TestApp#< Sinatra::Base
   set :environment, :production # so we don't get debug info that makes our test pass!
-  
+
   get %r{/redirect_to/(.*)} do
     redirect params[:captures]
   end
-  
+
   get '/host' do
     "current host is #{request.host}:#{request.port}, method get"
   end
-  
+
   get '/form_with_relative_action_to_host' do
     %{<form action="/host" method="post">
        <input type="submit" value="submit" />
       </form>}
   end
-  
+
   get '/relative_link_to_host' do
     %{<a href="/host">host</a>}
   end
-  
+
   post '/host' do
     "current host is #{request.host}:#{request.port}, method post"
   end
