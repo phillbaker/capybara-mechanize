2c0c37b4e4a4a2be539029df5b79765497a45f47
Jeroen van Dijk <jeroen@jeevidee.nl>
2011-07-11 15:33:08 +0200

    make these tests work in ruby 1.9

--- spec/driver/remote_mechanize_driver_spec.rb
+++ spec/driver/remote_mechanize_driver_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Capybara::Driver::Mechanize do
+  let(:ruby_19?) { ENV['RUBY_VERSION'] =~ /ruby-1.9/ }
+
   before(:each) do
     Capybara.app_host = REMOTE_TEST_URL
   end
@@ -22,19 +24,32 @@ describe Capybara::Driver::Mechanize do
     
     it "should pass arguments through to a get request" do
       @driver.visit("#{REMOTE_TEST_URL}/form/get", {:form => "success"})
-      @driver.body.should == %{<pre id="results">--- success\n</pre>}
+      
+      if ruby_19?
+        @driver.body.should == %{<pre id=\"results\">--- success\n...\n</pre>}
+      else
+        @driver.body.should == %{<pre id="results">--- success\n</pre>}
+      end
     end
 
     it "should pass arguments through to a post request" do
       @driver.post("#{REMOTE_TEST_URL}/form", {:form => "success"})
-      @driver.body.should == %{<pre id="results">--- success\n</pre>}
+      if ruby_19?
+        @driver.body.should == %{<pre id=\"results\">--- success\n...\n</pre>}
+      else
+        @driver.body.should == %{<pre id=\"results\">--- success\n</pre>}
+      end
     end
 
     context "for a post request" do
 
       it "should transform nested map in post data" do
         @driver.post("#{REMOTE_TEST_URL}/form", {:form => {:key => "value"}})
-        @driver.body.should == %{<pre id="results">--- \nkey: value\n</pre>}
+        if ruby_19?
+          @driver.body.should == %{<pre id=\"results">---\nkey: value\n<\/pre>}
+        else
+          @driver.body.should == %{<pre id="results">--- \nkey: value\n</pre>}
+        end
       end
 
     end
