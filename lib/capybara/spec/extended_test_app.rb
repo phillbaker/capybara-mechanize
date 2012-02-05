11d17e9899366aa6e7a77fa228f7a642a7f240ef
Matt Connolly <matt.connolly@me.com>
2012-02-05 22:51:29 +1000

    Adding another test for an app in a sub-domain via capybara session interface.

--- lib/capybara/spec/extended_test_app.rb
+++ lib/capybara/spec/extended_test_app.rb
@@ -35,6 +35,18 @@ class ExtendedTestApp < TestApp#< Sinatra::Base
     current_request_info
   end
 
+  get '/subsite/relative_link_to_host' do
+    %{<a href="/subsite/request_info2/host">host</a>}
+  end
+
+  get '/subsite/local_link_to_host' do
+    %{<a href="request_info2/host">host</a>}
+  end
+
+  get '/subsite/request_info2/*' do
+    "subsite: " + current_request_info
+  end
+
 
   private
 
