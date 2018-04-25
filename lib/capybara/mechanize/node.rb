e0ab3fe765d3eac6ee664eea75fbc4a7a0fed1d1
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-04-25 14:19:19 -0700

    Handle click arguments

--- lib/capybara/mechanize/node.rb
+++ lib/capybara/mechanize/node.rb
@@ -1,5 +1,7 @@
 class Capybara::Mechanize::Node < Capybara::RackTest::Node
-  def click
+  def click(keys = [], offset = {})
+    raise ArgumentError, "The mechanize driver does not support click options" unless keys.empty? && offset.empty?
+
     if tag_name == 'a'
       super
     elsif (tag_name == 'input' and %w(submit image).include?(type)) or
