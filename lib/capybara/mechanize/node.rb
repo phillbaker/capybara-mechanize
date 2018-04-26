02dc2a0d434c7bddba884a3a883732f368780e96
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-04-25 17:25:08 -0700

    Pull in changes from capybara rack-test

--- lib/capybara/mechanize/node.rb
+++ lib/capybara/mechanize/node.rb
@@ -2,12 +2,11 @@ class Capybara::Mechanize::Node < Capybara::RackTest::Node
   def click(keys = [], offset = {})
     raise ArgumentError, "The mechanize driver does not support click options" unless keys.empty? && offset.empty?
 
-    if tag_name == 'a'
-      super
-    elsif (tag_name == 'input' and %w(submit image).include?(type)) or
-        ((tag_name == 'button') and type.nil? or type == "submit")
+    if submits?
       associated_form = form
       Capybara::Mechanize::Form.new(driver, form).submit(self) if associated_form
+    else
+      super
     end
   end
 end
