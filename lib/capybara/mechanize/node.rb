50ac23f33373cb3c7d49d7eaf7ad1cdda6cffbe2
Phillip Baker <phillbaker@retrodict.com>
2018-05-12 14:45:17 -0400

    Include fixes for capybara 2.13.

--- lib/capybara/mechanize/node.rb
+++ lib/capybara/mechanize/node.rb
@@ -1,6 +1,7 @@
 class Capybara::Mechanize::Node < Capybara::RackTest::Node
   def click
-    if tag_name == 'a' or tag_name == 'label'
+    if tag_name == 'a' or tag_name == 'label' or
+        (tag_name == 'input' and %w(checkbox radio).include?(type))
       super
     elsif (tag_name == 'input' and %w(submit image).include?(type)) or
         ((tag_name == 'button') and type.nil? or type == "submit")
